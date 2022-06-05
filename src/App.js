251be497079dcc3cfb33e02ec5c3426cef3679bd
treeplus <yuky0@outlook.com>
2022-06-05 17:20:05 +0800

    a jsx example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
 
-function App() {
+const App = () => {
+  const now = new Date()
+  const a = 10
+  const b = 20
+
   return (
     <div className="App">
       <p>Hello world!</p>
+      <p>
+        {a} plus {b} is {a+b}
+      </p>
     </div>
   );
 }
