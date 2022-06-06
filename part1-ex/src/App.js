56b671e2753a53fa196bae1d5e030e627757c906
treeplus <yuky0@outlook.com>
2022-06-06 16:59:54 +0800

    part1 exercise

--- part1-ex/src/App.js
+++ part1-ex/src/App.js
@@ -1,3 +1,25 @@
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>{props.part} {props.exercise}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {}</p>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamental of React'
@@ -9,17 +31,10 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercise1}
-      </p>
-      <p>
-        {part2} {exercise2}
-      </p>
-      <p>
-        {part3} {exercise3}
-      </p>
-      <p>Number of exercises {exercise1 + exercise2 + exercise3}</p>
+      <Header course={course} />
+      <Content part={part1} exercise={exercise1} />
+      <Content part={part2} exercise={exercise2} />
+      <Content part={part3} exercise={exercise3} />
     </div>
   )
 }
