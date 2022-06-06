dfe7d27f6e6c90ae9bc24df24ceba3aef9bc43b9
treeplus <yuky0@outlook.com>
2022-06-06 09:22:25 +0800

    part1 exercise

---
+++ part1-ex/src/App.js
@@ -0,0 +1,27 @@
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamental of React'
+  const exercise1 = 10
+  const part2 = 'Using props to pass data'
+  const exercise2 = 7
+  const part3 = 'State of a component'
+  const exercise3 = 14
+
+  return (
+    <div>
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercise1}
+      </p>
+      <p>
+        {part2} {exercise2}
+      </p>
+      <p>
+        {part3} {exercise3}
+      </p>
+      <p>Number of exercises {exercise1 + exercise2 + exercise3}</p>
+    </div>
+  )
+}
+
+export default App;
